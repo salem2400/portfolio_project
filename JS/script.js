dd75b13d6850bb2e2d82a39194e6fca7e0fcb1ca
salem2400 <namperone9@gmail.com>
2024-09-18 23:47:44 +0300

    hi, i'm in bin

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,15 @@
+// Get references to the menu button and action element
 const menu = document.getElementById("menu");
 const action = document.getElementById("actions");
 
+// Add a click event listener to the menu button
 menu.addEventListener("click", () => {
-    hundelMenu();
+    hundelMenu(); // Call the function to handle menu toggle
 });
 
+// Function to handle the toggling of classes
 function hundelMenu(){
+    // Toggle the 'is-active' class on both the menu button and action element
     menu.classList.toggle("is-active");
     action.classList.toggle("is-active");
 }
